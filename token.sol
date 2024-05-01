327f5a9c9c2db158a78b0a4556a14e42b7471293
Tim <tim.eilers@protonmail.com>
2024-05-01 17:08:08 +0200

    finish luckyToken

--- token.sol
+++ token.sol
@@ -2,36 +2,45 @@
 
 pragma solidity >=0.6.12;
 
+import "./interfaces/IERC20.sol";
 import "./libraries/SafeMath.sol";
 
-contract LuckyCoin {
-    using SafeMath for uint;
+contract LuckyToken is IERC20 {
+    using SafeMath for uint256;
     //public keysword for state variables creates public getter but not setter
-    string public constant name = "LuckyCoin";
+    string public constant name = "LuckyToken";
     string public constant symbol = "LUCK";
     uint8 public constant decimals = 10; 
-    uint public totalSupply;
+    uint256 public totalSupply = 0;
+
     uint256 public transactionCount = 0; // Amount of transactions since last draw
     uint256 public constant lotteryThreshold = 10; // Amount of transaction to trigger a draw
     uint256 public constant lotteryFee = 10; // 10% fee for every transaction
     uint256 private _nonce = 0;
-    address private _lotteryTickets = new address[](lotteryThreshold);
+    address[] private _lotteryTickets = new address[](lotteryThreshold);
+    address private creator;
+
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
 
-    //mapping(address => uint) public nonces; //?
 
-    event Approval(address indexed owner, address indexed spender, uint value);
-    event Transfer(address indexed from, address indexed to, uint value);
-
-    constructor() public {
-        _mint(msg.sender, 777 * 10 ** uint256(decimals));
+    constructor() {
+        _mint(msg.sender, 777);
+        creator = msg.sender;
     }
     //why are some internal and some private?
     function _mint(address to, uint value) internal {
         totalSupply = totalSupply.add(value);
         balanceOf[to] = balanceOf[to].add(value);
         emit Transfer(address(0), to, value);
+    }
+
+    function mint(address to, uint value) external returns (bool) {
+        if(creator == msg.sender) {
+            _mint(to,value);
+            return true;
+        }
+        return false;
     }
 
     function _burn(address from, uint value) internal {
@@ -47,7 +56,7 @@ contract LuckyCoin {
 
     function _transfer(address from, address to, uint value) private {
         // Calculate fees paid by the user
-        uint256 fee = amount / lotteryFee;
+        uint256 fee = value / lotteryFee;
 
         // Transfer value without fee to receiver
         balanceOf[from] = balanceOf[from].sub(value);
@@ -83,7 +92,7 @@ contract LuckyCoin {
         address to,
         uint value
     ) external returns (bool) {
-        if (allowance[from][msg.sender] != uint(-1)) {
+        if (allowance[from][msg.sender] != type(uint256).max) {
             allowance[from][msg.sender] = allowance[from][msg.sender].sub(
                 value
             );
@@ -101,12 +110,12 @@ contract LuckyCoin {
         address winner = _lotteryTickets[i];
         uint256 value = balanceOf[address(0)];
         balanceOf[address(0)] = 0;
-        balanceOf[winner] = balanceOf[to].add(value);
+        balanceOf[winner] = balanceOf[winner].add(value);
 
         // Reset counter
         transactionCount = 0;
 
-        emit Transfer(address(0), to, value);
+        emit Transfer(address(0), winner, value);
     }
     
 }
