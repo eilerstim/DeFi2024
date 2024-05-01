127b3b176829a8695f7814acbed7739338f4cda3
Tim <tim.eilers@protonmail.com>
2024-05-01 14:07:18 +0200

    implemented lottery draw

--- token.sol
+++ token.sol
@@ -4,13 +4,18 @@ pragma solidity >=0.6.12;
 
 import "./libraries/SafeMath.sol";
 
-contract TnT_contract {
+contract LuckyCoin {
     using SafeMath for uint;
     //public keysword for state variables creates public getter but not setter
-    string public constant name = "TnT";
-    string public constant symbol = "TnT";
-    uint8 public constant decimals = 10; //not in use
+    string public constant name = "LuckyCoin";
+    string public constant symbol = "LUCK";
+    uint8 public constant decimals = 10; 
     uint public totalSupply;
+    uint256 public transactionCount; // Amount of transactions since last draw
+    uint256 public constant lotteryThreshold = 10; // Amount of transaction to trigger a draw
+    uint256 public constant lotteryFee = 10; // 10% fee for every transaction
+    uint256 private _nonce = 0;
+    address private _lotteryTickets = new address[](lotteryThreshold);
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
 
@@ -41,9 +46,26 @@ contract TnT_contract {
     }
 
     function _transfer(address from, address to, uint value) private {
+        // Calculate fees paid by the user
+        uint256 fee = amount / lotteryFee;
+
+        // Transfer value without fee to receiver
         balanceOf[from] = balanceOf[from].sub(value);
-        balanceOf[to] = balanceOf[to].add(value);
-        emit Transfer(from, to, value);
+        balanceOf[to] = balanceOf[to].add(value - fee);
+
+        // Fees are paid to 0x0
+        balanceOf[address(0)] = balanceOf[address(0)].add(fee);
+
+        // Add address to lottery tickets
+        _lotteryTickets[transactionCount] = from;
+        transactionCount++;
+
+        // Draw from lottery if threshold has been reached
+        if (transactionCount >= lotteryThreshold) {
+            _drawLottery();
+        }
+        emit Transfer(from, to, value - fee);
+        emit Transfer(from, address(0), fee);
     }
 
     function approve(address spender, uint value) external returns (bool) {
@@ -69,4 +91,22 @@ contract TnT_contract {
         _transfer(from, to, value);
         return true;
     }
+
+    function lotteryDraw() internal {
+        // Determine "random" index using hash function
+        _nonce++;
+        uint i = uint256(keccak256(abi.encodePacked(block.timestamp,msg.sender,_nonce))) % lotteryThreshold;
+
+        // Transfer from pool to winner
+        address winner = _lotteryTickets[i];
+        uint256 value = balanceOf[address(0)];
+        balanceOf[address(0)] = 0;
+        balanceOf[winner] = balanceOf[to].add(value);
+
+        // Reset counter
+        transactionCount = 0;
+
+        emit Transfer(address(0), to, value);
+    }
+    
 }
